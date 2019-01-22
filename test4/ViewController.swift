17900f6bb67b92137a08df591576ab33da4be87b
송은아 <song-eun-a@song-eun-aui-macbookpro.local>
2019-01-22 11:32:33 +0900

    Initial Commit

---
+++ test4/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  test4
+//
+//  Created by 송은아 on 22/01/2019.
+//  Copyright © 2019 송은아. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+    }
+    
+}
+
---
+++ test4/page2ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  page2ViewController.swift
+//  test4
+//
+//  Created by 송은아 on 22/01/2019.
+//  Copyright © 2019 송은아. All rights reserved.
+//
+
+import UIKit
+
+class page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
